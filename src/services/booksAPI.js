dc0ba81306c1eec081460e5480dc45792aa04c86
Kitsune4280 <ksokitsune@gmail.com>
2023-08-22 08:28:56 +0300

    db fix

--- src/services/booksAPI.js
+++ src/services/booksAPI.js
@@ -1,6 +1,7 @@
-//const URL = 'http://localhost:3000/db/books.json';
+const URL =
+	'https://kitsune4280.github.io/prometheus-x-course-task/db/books.json';
 
-const URL = './../db/books.json';
+//const URL = './../db/books.json';
 
 export const fetchBooks = () => {
 	return fetch(URL).then((resp) => resp.json());
